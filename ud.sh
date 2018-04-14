2454353c02122240a38fbc9571dc24252bcb17a8
gimpysticks <gimpysticks@gmail.com>
2018-04-13 20:21:30 -0400

    Added mounts.txt

--- ud.sh
+++ ud.sh
@@ -1,5 +1,5 @@
 #echo -e "PgFc"\!"qLr99\$B"|sudo -S apt-get update
 #echo -e "PgFc"\!"qLr99\$B"|sudo -S apt-get -y upgrade
-echo -e $stickspass|sudo -S apt-get update
+echo -e $stickspass|sudo -S apt-get -o Acquire::ForceIPv4=true update
 echo -e $stickspass|sudo -S apt-get -y upgrade
 echo -e $stickspass|sudo -S apt-get -y dist-upgrade
